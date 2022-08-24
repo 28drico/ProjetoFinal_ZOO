91df23a9dbf768e68e79662dc538cf8115cb7a44
Adriano-konig <konigadri87@hotmail.comgit config --global user.email konigadri87@hotmail.com>
2022-08-24 17:08:52 -0300

    Implementar logica bottoes instancia lista favorite e lista animal

--- app/src/main/java/br/com/zup/projetofinalindividual/ui/home/view/HomeActivity.kt
+++ app/src/main/java/br/com/zup/projetofinalindividual/ui/home/view/HomeActivity.kt
@@ -3,18 +3,46 @@ package br.com.zup.projetofinalindividual.ui.home.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import br.com.zup.projetofinalindividual.R
 import br.com.zup.projetofinalindividual.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var toolbar: Toolbar
+    private val navController : NavController by lazy { findNavController(R.id.nav_host_fragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_home)
-//        supportActionbar()
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toolbar = binding.toolbar
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
+
+      setupActionBarWithNavController(navController)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.ListaAnimal -> {
+                    navController.navigate(R.id.listaAnimalFragment)
+                    true
+                }
+                R.id.ListaFavorito -> {
+                    navController.navigate(R.id.listaFavoritoFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -28,9 +56,4 @@ class HomeActivity : AppCompatActivity() {
     private fun supportActionbar(){
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
     }
-
-//    private fun iniciarNavegacao(){
-//        val navHostFragment =
-//            supportFragmentManager.findFragmentById(binding.)
-//    }
 }
